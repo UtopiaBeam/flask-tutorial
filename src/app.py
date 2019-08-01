a1fc20a8fe0e1c35fec4a176aaa6e435c04e4c96
UtopiaBeam <utopiabeam@gmail.com>
2019-08-01 11:49:11 +0700

    :alembic: Try using environment

--- src/app.py
+++ src/app.py
@@ -1,14 +1,19 @@
+import os
+import sys
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/ping', methods=['GET'])
 def ping():
-    return jsonify({ 'message': 'pong' })
+    return jsonify({'message': 'pong'})
+
 
 @app.route('/ping', methods=['POST'])
 def message():
     return jsonify(request.get_json())
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host=os.getenv('HOST'), port=80, debug=True)
