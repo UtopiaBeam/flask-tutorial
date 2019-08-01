187c06450df3733bf8cac0072407d7f2ea231e5d
UtopiaBeam <utopiabeam@gmail.com>
2019-08-01 11:10:50 +0700

    :alembic: Try get request body

--- src/app.py
+++ src/app.py
@@ -1,10 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/ping', methods=['GET'])
 def ping():
     return jsonify({ 'message': 'pong' })
+
+@app.route('/ping', methods=['POST'])
+def message():
+    return jsonify(request.get_json())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
